9be135b8a1ac6af33bf21f76782eb04bd5f43b38
Alexander Shikov <aps98@mail.ru>
2022-05-30 20:56:30 +0300

    update

---
+++ figs/matlab/combine.m
@@ -0,0 +1,9 @@
+% 1 - no pa, 2 - pa
+% DFT-s-OFDM, 10 dBm TX, 64QAM, OBO 0 dB 
+L2 = findobj(2,'type','line');
+set(L2(2),'marker', '+')
+L = findobj(1,'type','line');
+set(L(2),'color','r')
+copyobj(L,findobj(2,'type','axes'));
+
+legend("УМ включен", "Изначальное созвездие","УМ выключен",'Location','southeast')
